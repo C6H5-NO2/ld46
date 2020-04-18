ccab200a6ff5656e3f9e7b750eaa497944639637
C6H5-NO2 <19368807+C6H5-NO2@users.noreply.github.com>
2020-04-18 23:55:53 +0800

    Day1 Progress
    It's a bit behind schedule.

--- Assets/Scripts/GenGrid.cs
+++ Assets/Scripts/GenGrid.cs
@@ -6,17 +6,55 @@ public class GenGrid : MonoBehaviour {
     public GameObject floorPrefab;
     public Vector2Int gridSize = new Vector2Int(10, 10);
 
+    private List<GameObject> floors = new List<GameObject>();
+
+    public GameObject GetFloorAt(int x, int y) { return floors[x * gridSize.y + y]; }
+    public GameObject GetFloorAt(Vector2Int pos) { return GetFloorAt(pos.x, pos.y); }
+
+    [ContextMenu("InitFloor")]
     private void InitFloor() {
+        StartCoroutine(DoInitFloor());
+        //StartCoroutine(smoothMoveUp());
+    }
+
+    private void Start() {
+        InitFloor();
+    }
+
+    private void Update() {
+
+    }
+
+    private IEnumerator DoInitFloor() {
         var floorHolder = new GameObject("FloorHolder").transform;
         floorHolder.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
         for(var x = 0; x < gridSize.x; ++x)
             for(var z = 0; z < gridSize.y; ++z) {
                 var floor = Instantiate(floorPrefab, floorHolder);
                 floor.transform.localPosition = new Vector3(x, 0, z);
+                floors.Add(floor);
+                yield return null;
             }
     }
 
-    private void Start() {
-        InitFloor();
-    }
+    // too slow
+    //private IEnumerator smoothMoveUp() {
+    //    var hasmove = true;
+    //    while(hasmove) {
+    //        hasmove = false;
+    //        for(var x = 0; x < gridSize.x; ++x)
+    //            for(var z = 0; z < gridSize.y; ++z) {
+    //                var trans = GetFloorAt(x, z).transform;
+    //                var targetPos = trans.position;
+    //                targetPos.y = 0;
+    //                if((trans.position - targetPos).sqrMagnitude > 1e-3) {
+    //                    trans.position = Vector3.Lerp(trans.position, targetPos, .7f);
+    //                    hasmove = true;
+    //                    yield return null; //new WaitForSeconds(.4f);
+    //                }
+    //                else
+    //                    trans.position = targetPos;
+    //            }
+    //    }
+    //}
 }
---
+++ Assets/Scripts/MoveCat.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCat : MonoBehaviour {
+    public Material dst, src;
+    public GenGrid genGrid;
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            var cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(cameraRay, out RaycastHit hitInfo, 100, LayerMask.GetMask("Floor"))) {
+                var pos = new Vector3Int((int)hitInfo.point.x, 0, (int)hitInfo.point.z);
+                Debug.Log(hitInfo.point + " " + pos);
+
+                transform.position = pos;
+                // testcode
+                // todo: a toggled floor can not be toggled again
+                StartCoroutine(ToggleFloorTile(genGrid.GetFloorAt(pos.x, pos.z).GetComponentInChildren<Renderer>()));
+            }
+            else {
+                Debug.Log("no hit");
+            }
+        }
+    }
+
+    private IEnumerator ToggleFloorTile(Renderer floorRenderer) {
+        var srcMat = floorRenderer.material;
+        floorRenderer.material = dst;
+        yield return new WaitForSeconds(1);
+        floorRenderer.material = srcMat;
+    }
+}
